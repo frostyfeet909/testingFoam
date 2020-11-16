c8182e2cc02a0b07c9af155ea2eb4a8bd9ad88ac
frostyfeet909 <algiesampson@gmail.com>
2020-11-16 16:57:30 +0000

    Removing recursion

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # Wrapper for all applications
 import os
+from time import time
 
 
-def main(base_path):
+def main():
+    print("\n")
     print("Welcome!")
     print("\n")
     print("Choose you application:")
@@ -10,53 +12,55 @@ def main(base_path):
     print("2 - generateData")
     print("3 - plotCases")
     print("4 - plotColor")
-    choice = raw_input(">> ")
 
-    if choice == "1":
-        try:
-            from findGood import main as app_main
-        except:
-            print("[!!] Could not find findGood")
+    choice = 1
+    base_path = os.path.dirname(os.path.realpath(__file__))
+
+    while choice != "" and choice != "q" and choice != "exit" and choice != "stop":
+        choice = raw_input(">> ")
+
+        elapsed = time()
+        if choice == "1":
+            try:
+                from findGood import main as app_main
+                os.chdir("findGood")
+                app_main.main()
+            except ImportError or OSError:
+                print("[!!] Could not find findGood")
+
+        elif choice == "2":
+            try:
+                from generateData import main as app_main
+                os.chdir("generateData")
+                app_main.main()
+            except ImportError or OSError:
+                print("[!!] Could not find generateData")
+
+        elif choice == "3":
+            try:
+                from plotCases import main as app_main
+                os.chdir("plotCases")
+                app_main.main()
+            except ImportError or OSError:
+                print("[!!] Could not find plotCases")
+
+        elif choice == "4":
+            try:
+                from plotColor import main as app_main
+                os.chdir("plotColor")
+                app_main.main()
+            except ImportError or OSError:
+                print("[!!] Could not find plotColor")
+
+        else:
+            print("[!!] Bye!")
             raise SystemExit
 
-        os.chdir("findGood")
-
-    elif choice == "2":
-        try:
-            from generateData import main as app_main
-        except:
-            print("[!!] Could not find generateData")
-            raise SystemExit
-
-        os.chdir("generateData")
-
-    elif choice == "3":
-        try:
-            from plotCases import main as app_main
-        except:
-            print("[!!] Could not find plotCases")
-            raise SystemExit
-
-        os.chdir("plotCases")
-
-    elif choice == "4":
-        try:
-            from plotColor import main as app_main
-        except:
-            print("[!!] Could not find plotColor")
-            raise SystemExit
-
-        os.chdir("plotColor")
-
-    else:
-        print("[!!] Bye!")
-        raise SystemExit
-
-    app_main.main()
-    os.chdir(base_path)
-    main(base_path)  # Recursion
+        os.chdir(base_path)
+        elapsed = time() - elapsed
+        print("That took %s minutes" % str(round(elapsed / 60)))
+        print("\n")
 
 
 if __name__ == "__main__":
-    base = os.getcwd()
-    main(base)
+    main()
